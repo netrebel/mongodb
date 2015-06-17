bfa8e2d8710255d8963e3ad35fe69782d9d56290
Miguel Reyes <miguel.reyes@millicom.com>
2015-06-17 17:35:15 +0200

    Homework 3.1: Second implementation. Iterate school.students and delete homework with lowest score.

--- M101J/src/main/java/com/miguel/mongodb/Homework_3_1.java
+++ M101J/src/main/java/com/miguel/mongodb/Homework_3_1.java
@@ -23,49 +23,42 @@ public class Homework_3_1 {
 
         MongoDatabase schoolDatabase = client.getDatabase("school");
         MongoCollection<Document> studentsCollection = schoolDatabase.getCollection("students");
-
-        Document projection = new Document()
-                .append("student_id", 1);
-
         FindIterable<Document> students = studentsCollection.find();
 
         for (Document student : students) {
-            ArrayList<Document> scores = student.get("scores", ArrayList.class);
-            Document homework1 = scores.get(2);
-            Document homework2 = scores.get(3);
+            List<Document> scores = student.get("scores", List.class);
+            Document homework1 = null;
+            Document homework2;
 
             List<Document> newList = new ArrayList<Document>();
-            newList.add(scores.get(0));
-            newList.add(scores.get(1));
 
-            if (homework1.getDouble("score") > homework2.getDouble("score")) {
-                newList.add(homework1);
-            } else {
-                newList.add(homework2);
-            }
-
-            student.put("scores", newList);
-            Document query = new Document("_id", student.getDouble("_id"));
-            Document update = new Document("$set", new Document("scores", newList));
-            studentsCollection.updateOne(query, update);
-
-            /*java.util.List<Document> newScores = new ArrayList<Document>();
-            for (int i = 0; i < scores.size(); i++) {
-                Document document = scores.get(i);
-                if (document.get("type").equals("homework")) {
-                    score = document.getDouble("score");
-                } else {
-                    newScores.add(document);
-                    newScores.add(document);
+            boolean firstOneFound = false;
+            if (scores.size() > 3) {
+                for (int i = 0; i < scores.size(); i++) {
+                    Document document = scores.get(i);
+                    if (document.get("type").equals("homework") && !firstOneFound) {
+                        firstOneFound = true;
+                        homework1 = document;
+                    } else {
+                        if (firstOneFound) {
+                            homework2 = document;
+                            if (homework1.getDouble("score") > homework2.getDouble("score")) {
+                                newList.add(homework1);
+                            } else {
+                                newList.add(homework2);
+                            }
+                        } else {
+                            newList.add(document);
+                        }
+                    }
                 }
-            }*/
-            System.out.println("Found studentId: " + student.getDouble("_id"));
 
+                student.put("scores", newList);
+                Document query = new Document("_id", student.getDouble("_id"));
+                Document update = new Document("$set", new Document("scores", newList));
+                studentsCollection.updateOne(query, update);
+                System.out.println("Updated studentId: " + student.getDouble("_id"));
+            }
         }
-
-
-//                studentsCollection.deleteOne(first);
-
-
     }
 }
